1f00b4af2293fd4e1cad8fc4258d832d63a77056
jamesholland-oss <james.holland@swishbreaks.com>
2026-08-21 16:55:15 -0400

    Expose Swish Control app configuration bridge

--- preload.js
+++ preload.js
@@ -2,5 +2,8 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('swish', {
   getStreams: () => ipcRenderer.invoke('streams:get'),
-  saveStreams: (streams) => ipcRenderer.invoke('streams:save', streams)
+  saveStreams: (streams) => ipcRenderer.invoke('streams:save', streams),
+  getAppConfig: () => ipcRenderer.invoke('app:get-config'),
+  saveAppConfig: (patch) => ipcRenderer.invoke('app:save-config', patch),
+  restartApp: () => ipcRenderer.invoke('app:restart')
 });
